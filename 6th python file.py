8873c73e7f297bdb6d2576a59536b8d3208d439d
oct12aanya <125890573+oct12aanya@users.noreply.github.com>
2023-03-06 18:43:27 +0530

    Add files via upload

---
+++ 6th python file.py
@@ -0,0 +1,5 @@
+print ("This is a program to generate multiplycation tables")
+n=int(input("Enter the number for printing out its multiplycation table"))
+
+for i in range(1,11): #For loop for four iterations
+    print(n," x ",i," = ", i*n) #Printing out the table
